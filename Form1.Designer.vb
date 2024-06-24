c4ba28ed51c515b528bd1693f5741eef35b88ce8
deekshitha <d.madala@iitg.ac.in>
2024-06-24 10:18:10 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label1 = New Label()
+        Label3 = New Label()
+        PlotView1 = New OxyPlot.WindowsForms.PlotView()
+        PlotView2 = New OxyPlot.WindowsForms.PlotView()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        samplingRate = New TextBox()
+        hopLength = New TextBox()
+        frame = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(16, 18)
+        Button1.Margin = New Padding(4, 5, 4, 5)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(147, 35)
+        Button1.TabIndex = 0
+        Button1.Text = "Load Data"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(16, 63)
+        Button2.Margin = New Padding(4, 5, 4, 5)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(147, 35)
+        Button2.TabIndex = 1
+        Button2.Text = "Compute STFT"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(171, 26)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(89, 20)
+        Label1.TabIndex = 3
+        Label1.Text = "Loaded File:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(268, 26)
+        Label3.Margin = New Padding(4, 0, 4, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(0, 20)
+        Label3.TabIndex = 5
+        ' 
+        ' PlotView1
+        ' 
+        PlotView1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        PlotView1.Location = New Point(16, 108)
+        PlotView1.Margin = New Padding(4, 5, 4, 5)
+        PlotView1.Name = "PlotView1"
+        PlotView1.PanCursor = Cursors.Hand
+        PlotView1.Size = New Size(1035, 242)
+        PlotView1.TabIndex = 6
+        PlotView1.Text = "PlotView1"
+        PlotView1.ZoomHorizontalCursor = Cursors.SizeWE
+        PlotView1.ZoomRectangleCursor = Cursors.SizeNWSE
+        PlotView1.ZoomVerticalCursor = Cursors.SizeNS
+        ' 
+        ' PlotView2
+        ' 
+        PlotView2.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        PlotView2.Location = New Point(16, 108)
+        PlotView2.Margin = New Padding(4, 5, 4, 5)
+        PlotView2.Name = "PlotView2"
+        PlotView2.PanCursor = Cursors.Hand
+        PlotView2.Size = New Size(1035, 669)
+        PlotView2.TabIndex = 7
+        PlotView2.Text = "PlotView2"
+        PlotView2.ZoomHorizontalCursor = Cursors.SizeWE
+        PlotView2.ZoomRectangleCursor = Cursors.SizeNWSE
+        PlotView2.ZoomVerticalCursor = Cursors.SizeNS
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(176, 71)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(109, 20)
+        Label4.TabIndex = 8
+        Label4.Text = "Sampling Rate:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(446, 71)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(80, 20)
+        Label5.TabIndex = 9
+        Label5.Text = "frame size:"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(698, 71)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(70, 20)
+        Label6.TabIndex = 10
+        Label6.Text = "Hop size:"
+        ' 
+        ' samplingRate
+        ' 
+        samplingRate.Location = New Point(291, 71)
+        samplingRate.Name = "samplingRate"
+        samplingRate.Size = New Size(125, 27)
+        samplingRate.TabIndex = 11
+        samplingRate.Text = "16000"
+        ' 
+        ' hopLength
+        ' 
+        hopLength.Location = New Point(774, 68)
+        hopLength.Name = "hopLength"
+        hopLength.Size = New Size(125, 27)
+        hopLength.TabIndex = 12
+        hopLength.Text = "80"
+        ' 
+        ' frame
+        ' 
+        frame.Location = New Point(532, 71)
+        frame.Name = "frame"
+        frame.Size = New Size(125, 27)
+        frame.TabIndex = 13
+        frame.Text = "320"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1067, 797)
+        Controls.Add(frame)
+        Controls.Add(hopLength)
+        Controls.Add(samplingRate)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(PlotView2)
+        Controls.Add(PlotView1)
+        Controls.Add(Label3)
+        Controls.Add(Label1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Margin = New Padding(4, 5, 4, 5)
+        Name = "Form1"
+        Text = "STFT"
+        ResumeLayout(False)
+        PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PlotView1 As OxyPlot.WindowsForms.PlotView
+    Friend WithEvents PlotView2 As OxyPlot.WindowsForms.PlotView
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents samplingRate As TextBox
+    Friend WithEvents hopLength As TextBox
+    Friend WithEvents frame As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,188 @@
+﻿Imports System.IO
+Imports System.Numerics
+Imports OxyPlot
+Imports OxyPlot.Axes
+Imports OxyPlot.Series
+Imports OxyPlot.WindowsForms
+
+Public Class Form1
+    Private integerList As New List(Of Double)()
+    Private stftMatrix As Complex(,)
+    Private stftMag As Double(,)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        ' Set the filter for file types
+        openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+
+        If openFileDialog1.ShowDialog() = DialogResult.OK Then
+            ' Get the path of the selected file
+            Dim filePath As String = openFileDialog1.FileName
+
+            ' Clear the previous data
+            integerList.Clear()
+
+            Label3.Text = Path.GetFileName(filePath)
+
+            ' Read the file line by line
+            Try
+                Using sr As New StreamReader(filePath)
+                    Dim line As String
+                    line = sr.ReadLine()
+                    While line IsNot Nothing
+                        Dim number As Double
+                        If Double.TryParse(line, number) Then
+                            integerList.Add(number)
+                        End If
+                        line = sr.ReadLine()
+                    End While
+                End Using
+
+                PlotOriginalDataOnPlotView()
+            Catch ex As Exception
+                MessageBox.Show("Error reading file: " & ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Compute and plot the STFT
+        ComputeSTFT()
+        PlotSTFTMagnitudesOnPlotView()
+    End Sub
+
+    Private Sub ComputeSTFT()
+        Dim fs As Integer = samplingRate.Text  ' Sample rate (Hz), adjust according to your data
+        Dim frameSize As Integer = frame.Text
+        Dim hopSize As Integer = hopLength.Text
+
+        Dim signal As Double() = integerList.ToArray()
+        stftMatrix = ComputeSTFT(signal, fs, frameSize, hopSize)
+    End Sub
+
+    Private Function ComputeSTFT(signal As Double(), fs As Integer, frameSize As Integer, hopSize As Integer) As Complex(,)
+        ' Number of frames
+        Dim numFrames As Integer = Math.Ceiling((signal.Length - frameSize) / hopSize) + 1
+
+        ' Pad the signal to make sure all frames fit within the signal length
+        Dim padLength As Integer = (numFrames - 1) * hopSize + frameSize
+        Dim paddedSignal(padLength - 1) As Double
+        Array.Copy(signal, paddedSignal, signal.Length)
+
+        ' Initialize the STFT matrix
+        Dim stftMatrix(256 - 1, numFrames - 1) As Complex
+
+        ' Compute STFT
+        For i As Integer = 0 To numFrames - 1
+            Dim startIdx As Integer = i * hopSize
+            Dim frame(frameSize - 1) As Double
+            For j As Integer = 0 To frameSize - 1
+                frame(j) = paddedSignal(startIdx + j)
+            Next
+
+            Dim frameDFT As Complex() = DFT(frame)
+            Dim sb As New System.Text.StringBuilder()
+            For k As Integer = 0 To 256 - 1
+                stftMatrix(k, i) = frameDFT(k)
+            Next
+        Next
+
+        Return stftMatrix
+    End Function
+
+    Private Function DFT(y As Double()) As Complex()
+        Dim N As Integer = y.Length
+        Dim dftResult(N - 1) As Complex
+
+        For k As Integer = 0 To 256 - 1
+            Dim sum As Complex = Complex.Zero
+            For innerN As Integer = 0 To N - 1
+                Dim angle As Double = -2.0 * Math.PI * k * innerN / N
+                sum += y(innerN) * New Complex(Math.Cos(angle), Math.Sin(angle))
+            Next
+            dftResult(k) = sum
+        Next
+
+        Return dftResult
+    End Function
+
+    Private Sub PlotOriginalDataOnPlotView()
+        Dim model As New PlotModel With {
+            .Title = "Original Data Plot"
+        }
+
+        Dim xAxis As New LinearAxis() With {
+            .Position = AxisPosition.Bottom,
+            .Title = "Time(ms)"
+        }
+        Dim yAxis As New LinearAxis() With {
+            .Position = AxisPosition.Left,
+            .Title = "Amplitude"
+        }
+
+        model.Axes.Add(xAxis)
+        model.Axes.Add(yAxis)
+
+        Dim series As New LineSeries()
+        For i As Integer = 0 To integerList.Count - 1
+            series.Points.Add(New DataPoint(i / 16, integerList(i)))
+        Next
+
+        model.Series.Add(series)
+
+        PlotView1.Model = model
+    End Sub
+
+    Private Sub PlotSTFTMagnitudesOnPlotView()
+        Dim model As New PlotModel With {
+            .Title = "STFT Magnitudes Plot"
+        }
+
+        Dim heatMapSeries As New OxyPlot.Series.HeatMapSeries()
+        Dim rows As Integer = stftMatrix.GetLength(0)
+        Dim cols As Integer = stftMatrix.GetLength(1)
+        Dim magnitudes(rows - 1, cols - 1) As Double
+
+        For i As Integer = 0 To rows - 1
+            For j As Integer = 0 To cols - 1
+                magnitudes(i, j) = stftMatrix(i, j).Magnitude
+            Next
+        Next
+
+        heatMapSeries.X0 = 0
+        heatMapSeries.X1 = cols
+        heatMapSeries.Y0 = 0
+        heatMapSeries.Y1 = rows
+        heatMapSeries.Interpolate = True
+        heatMapSeries.Data = magnitudes
+
+        model.Series.Add(heatMapSeries)
+
+        ' Add axes
+        model.Axes.Add(New LinearColorAxis() With {
+            .Position = AxisPosition.Right,
+            .Palette = OxyPalettes.Gray(256)
+        })
+
+        PlotView2.Model = model
+    End Sub
+    Private Function STFTMatrixToString(matrix As Complex(,)) As String()
+        Dim rows As Integer = matrix.GetLength(0)
+        Dim cols As Integer = matrix.GetLength(1)
+        Dim result(rows - 1) As String
+
+        For i As Integer = 0 To rows - 1
+            Dim sb As New System.Text.StringBuilder()
+
+            For j As Integer = 0 To cols - 1
+                sb.Append(matrix(i, j).Magnitude.ToString("F2") & " ")
+            Next
+
+            result(i) = sb.ToString()
+        Next
+
+        Return result
+    End Function
+
+End Class
